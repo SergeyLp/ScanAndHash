70efab340912a59041d8fa77a943ec7072f0dacb
Sergej <lipsergey@gmail.com>
2017-01-02 14:04:59 +0300

    Scan one dir

---
+++ scan.py
@@ -0,0 +1,20 @@
+'''
+Отыскивает наибольший файл с исходным программным кодом на языке Python
+в единственном каталоге.
+Поиск выполняется в каталоге стандартной биб­лиотеки Python для Windows, если
+в аргументе командной строки не был указан какой-то другой каталог.
+'''
+
+import os, glob, sys
+
+dirname = r'/media/win_hdd/_Data/Text/En/Sub' if len(sys.argv) == 1 else sys.argv[1]
+allsizes = []
+
+allpy = glob.glob(dirname + os.sep + '*.srt')
+for filename in allpy:
+    filesize = os.path.getsize(filename)
+    allsizes.append((filesize, filename))
+allsizes.sort()
+
+print(allsizes[:2])
+print(allsizes[-2:])
