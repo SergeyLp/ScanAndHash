aa98bb4bee6708fcab641fc6050b822dd96a7f71
Sergej <lipsergey@gmail.com>
2017-01-02 19:36:57 +0300

    endswith more comprehand

--- scan.py
+++ scan.py
@@ -1,20 +1,25 @@
 import sys, os, pprint
 
-trace = 0
+trace = 0 # 1=каталоги, 2=+файлы
 dirname = r'./Texts' if len(sys.argv) == 1 else sys.argv[1]
 
 allsizes = []
 for (thisDir, subsHere, filesHere) in os.walk(dirname, followlinks=True):
     if trace: print(thisDir)
     for filename in filesHere:
-        if filename.endswith('.txt'):
-            if trace: print('...', filename)
+        if filename.endswith(('.txt','.srt')):    ## or filename.endswith('.srt'):
+            if trace > 1: print('...', filename)
             fullname = os.path.join(thisDir, filename)
-            fullsize = os.path.getsize(fullname)
-            allsizes.append((fullsize, fullname))
+            try:
+                txtsize = os.path.getsize(fullname)
+            except os.error:
+                print('skipping', fullname, sys.exc_info()[0])
+            else:
+                lines = len(open(fullname, 'rb').readlines())
+                allsizes.append((txtsize, lines, filename))
 allsizes.sort()
-pprint.pprint(allsizes[:2])
-pprint.pprint(allsizes[-2:])
+pprint.pprint(allsizes[:3])
+pprint.pprint(allsizes[-3:])
 
 
 
