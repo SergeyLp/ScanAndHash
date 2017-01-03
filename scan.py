c106e6b16a895494374916458cd0ae52a5a8d753
Martin <Martin>
2017-01-03 12:05:11 +0300

    Change to use generator functions

--- scan.py
+++ scan.py
@@ -1,25 +1,30 @@
-import sys, os, pprint
+import sys, os, pprint, re
 
-trace = 0 # 1=каталоги, 2=+файлы
 dirname = r'./Texts' if len(sys.argv) == 1 else sys.argv[1]
 
+def gen_lines(fullname):
+    with open(fullname) as data:  # open(file, encoding="cp866")
+        for line in data:
+            line = line.strip() # TODO: Убрать!
+            if len(line) > 0:
+                yield line
+            '''TODO: Склеивать в абзац, определять пунктуацию'''
+
+
+def gen_filelist():
+    for (thisDir, subsHere, filesHere) in os.walk(dirname, followlinks=True):
+        for filename in filesHere:
+            if filename.endswith(('.txt','.srt')):
+                fullname = os.path.join(thisDir, filename)
+                yield fullname
+                                           
+
 allsizes = []
-for (thisDir, subsHere, filesHere) in os.walk(dirname, followlinks=True):
-    if trace: print(thisDir)
-    for filename in filesHere:
-        if filename.endswith(('.txt','.srt')):    ## or filename.endswith('.srt'):
-            if trace > 1: print('...', filename)
-            fullname = os.path.join(thisDir, filename)
-            try:
-                txtsize = os.path.getsize(fullname)
-            except os.error:
-                print('skipping', fullname, sys.exc_info()[0])
-            else:
-                lines = len(open(fullname, 'rb').readlines())
-                allsizes.append((txtsize, lines, filename))
-allsizes.sort()
-pprint.pprint(allsizes[:3])
-pprint.pprint(allsizes[-3:])
+lines = []
 
+for fname in gen_filelist():
+    print(fname)
+    for line in gen_lines(fname):
+        lines.append(line)
 
-
+pprint.pprint(lines[:15])
