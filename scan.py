cd02e73690f21d2fa2fec61ef2910c444e7ffba7
Martin <Martin>
2017-01-03 12:26:26 +0300

    Generate words

--- scan.py
+++ scan.py
@@ -2,13 +2,16 @@ import sys, os, pprint, re
 
 dirname = r'./Texts' if len(sys.argv) == 1 else sys.argv[1]
 
+def gen_wordslist(line: str):    
+    yield line.split()
+
+
 def gen_lines(fullname):
     with open(fullname) as data:  # open(file, encoding="cp866")
         for line in data:
             line = line.strip() # TODO: Убрать!
             if len(line) > 0:
                 yield line
-            '''TODO: Склеивать в абзац, определять пунктуацию'''
 
 
 def gen_filelist():
@@ -17,14 +20,20 @@ def gen_filelist():
             if filename.endswith(('.txt','.srt')):
                 fullname = os.path.join(thisDir, filename)
                 yield fullname
-                                           
 
-allsizes = []
-lines = []
+
+words = []
 
 for fname in gen_filelist():
-    print(fname)
     for line in gen_lines(fname):
-        lines.append(line)
+        for wordlist in gen_wordslist(line):
+            ##words.append(word)
+            #words += word
+            words = [*words, *wordlist]
 
-pprint.pprint(lines[:15])
+print(words[:40])
+pprint.pprint(words[:13])
+'''
+TODO: Склеивать в абзац, определять пунктуацию
+Reading varias coding of text
+'''
